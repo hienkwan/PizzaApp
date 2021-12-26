9e2dcedf6049986f4a8b49fac6abcd3871bfb491
HienKwan <hienq127@gmail.com>
2021-12-26 12:07:58 +0700

    Add find Bill by date in Bill controller

--- src/main/java/com/example/PizzaApp/controller/BillController.java
+++ src/main/java/com/example/PizzaApp/controller/BillController.java
@@ -1,9 +1,30 @@
 package com.example.PizzaApp.controller;
 
+import com.example.PizzaApp.model.Bill;
+import com.example.PizzaApp.model.Category;
+import com.example.PizzaApp.service.BillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class BillController {
+    @Autowired
+    private BillService billService;
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(value = "/billsWithDate/{date}", method = RequestMethod.GET)
+    public ResponseEntity<List<Bill>> getAllBillWithDate(String date) {
+        LocalDate dateToFind = LocalDate.parse(date);
+        List<Bill> billList = billService.findBillWithDate(dateToFind);
+        return new ResponseEntity<>(billList, HttpStatus.OK);
+    }
 }
