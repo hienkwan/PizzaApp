2ee5b3a911a7b290158de204bf6c7e346d612b34
HienKwan <hienq127@gmail.com>
2022-01-05 09:56:42 +0700

    Add bill controller

--- src/main/java/com/example/PizzaApp/controller/BillController.java
+++ src/main/java/com/example/PizzaApp/controller/BillController.java
@@ -1,12 +1,13 @@
 package com.example.PizzaApp.controller;
 
+import com.example.PizzaApp.dto.BillDto;
 import com.example.PizzaApp.model.Bill;
-import com.example.PizzaApp.model.Category;
 import com.example.PizzaApp.service.BillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,12 @@ public class BillController {
         LocalDate dateToFind = LocalDate.parse(date);
         List<Bill> billList = billService.findBillWithDate(dateToFind);
         return new ResponseEntity<>(billList, HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
+    @RequestMapping(value = "/bill", method = RequestMethod.POST)
+    public ResponseEntity<Bill> createBill(@RequestBody BillDto billDto) {
+        Bill billSaved = billService.createBill(billDto);
+        return new ResponseEntity<>(billSaved, HttpStatus.OK);
     }
 }
