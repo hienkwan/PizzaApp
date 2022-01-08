3f164be0834b37871031ea3feb09a5efb4d44015
HienKwan <hienq127@gmail.com>
2022-01-08 11:18:24 +0700

    Add customer service

--- src/main/java/com/example/PizzaApp/service/CustomerService.java
+++ src/main/java/com/example/PizzaApp/service/CustomerService.java
@@ -1,4 +1,7 @@
 package com.example.PizzaApp.service;
 
+import com.example.PizzaApp.model.Customer;
+
 public interface CustomerService {
+    String signup(Customer customer);
 }
--- src/main/java/com/example/PizzaApp/service/CustomerServiceImpl.java
+++ src/main/java/com/example/PizzaApp/service/CustomerServiceImpl.java
@@ -1,4 +1,26 @@
 package com.example.PizzaApp.service;
 
+import com.example.PizzaApp.model.Customer;
+import com.example.PizzaApp.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import static com.example.PizzaApp.utils.MD5.getMd5;
+
+@Service
 public class CustomerServiceImpl implements CustomerService{
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+
+    @Override
+    public String signup(Customer customer) {
+        String hash ="ABC123456789";
+        String password = customer.getPassword();
+        String passwordToMd5 = getMd5(password);
+        customer.setPassword(passwordToMd5);
+        Customer customerSaved = customerRepository.save(customer);
+        return customerSaved.getUsername();
+    }
 }
